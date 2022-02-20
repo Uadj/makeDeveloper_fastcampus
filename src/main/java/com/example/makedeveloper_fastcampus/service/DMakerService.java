9713fb177c6ed308cb1760fdbeeb35293d67623a
Gyuwon DoUADJ <ds257873@naver.com>
2022-02-20 21:37:39 +0900

    Business Validation about Create Developer api

--- src/main/java/com/example/makedeveloper_fastcampus/service/DMakerService.java
+++ src/main/java/com/example/makedeveloper_fastcampus/service/DMakerService.java
@@ -2,6 +2,8 @@ package com.example.makedeveloper_fastcampus.service;
 
 import com.example.makedeveloper_fastcampus.dto.CreateDeveloper;
 import com.example.makedeveloper_fastcampus.entity.Developer;
+import com.example.makedeveloper_fastcampus.exception.DMakerErrorCode;
+import com.example.makedeveloper_fastcampus.exception.DMakerException;
 import com.example.makedeveloper_fastcampus.repository.DeveloperRepository;
 import com.example.makedeveloper_fastcampus.type.DeveloperLevel;
 import com.example.makedeveloper_fastcampus.type.DeveloperSkillType;
@@ -9,6 +11,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+
+import java.util.Optional;
+
+import static com.example.makedeveloper_fastcampus.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static com.example.makedeveloper_fastcampus.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEAR_NOT_MATCHED;
+import static org.springframework.data.repository.util.ClassUtils.ifPresent;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +34,25 @@ public class DMakerService {
                 .build();
         developerRepository.save(developer);
         return CreateDeveloper.Response.fromEntity(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request){
+
+        DeveloperLevel DeveloperLevel = request.getDeveloperLevel();
+        Integer experienceYear = request.getExperienceYears();
+        if(DeveloperLevel == DeveloperLevel.SENIOR && experienceYear < 10){
+            throw new DMakerException(LEVEL_EXPERIENCE_YEAR_NOT_MATCHED);
+        }
+        if(DeveloperLevel == DeveloperLevel.JUNGIOR && (experienceYear < 4 || experienceYear >10)){
+            throw new DMakerException(LEVEL_EXPERIENCE_YEAR_NOT_MATCHED);
+        }
+        if(DeveloperLevel == DeveloperLevel.JUNIOR && experienceYear > 4){
+            throw new DMakerException(LEVEL_EXPERIENCE_YEAR_NOT_MATCHED);
+        }
+
+        developerRepository.findByMemberId(request.getMemberId())
+        .ifPresent((developer->{
+            throw new DMakerException(DUPLICATED_MEMBER_ID);
+        }));
     }
 }
